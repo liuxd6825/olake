1e015f967f03849a4544c66f41bf9eff4bda0e39
duxm0114 <duxm0114@163.com>
2025-10-29 16:10:17 +0800

    增加mongo数据库cdc支持,支持领域事件模式

---
+++ build-bin.sh
@@ -0,0 +1,3 @@
+GOOS=linux GOARCH=amd64 go build -o ./dist/linux/amd64/olake-mongodb ./drivers/mongodb/main.go
+# GOOS=linux GOARCH=amd64 go build -o ./dist/linux/amd64/olake-mysql ./drivers/mysql/main.go
+# GOOS=linux GOARCH=amd64 go build -o ./dist/linux/amd64/olake-postgres ./drivers/postgres/main.go
